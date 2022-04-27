81904a6ce95d33d648936bb1f9d345c7e0898461
TommyN987 <tomas.nagy.tn@gmail.com>
2022-04-27 16:20:50 +0200

    add activeTab state and conditional tab styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -9,10 +9,25 @@ const Content = () => {
   return (
     <div className="content-container">
       <div className="tabs">
-        <div className="tab-leagues" onClick={() => setActiveTab('leagues')}>
+        <div 
+          className="tab-leagues" 
+          onClick={() => setActiveTab('leagues')} style={
+          {
+            color: activeTab === 'leagues' ? 'var(--secondary-color)' : null,
+            backgroundColor: activeTab === 'leagues' ? 'var(--primary-color)' : null,
+            border: activeTab === 'leagues' ? '2px solid var(--secondary-color' : null
+          }}>
           <h2>Leagues</h2>
         </div>
-        <div className="tab-standings" onClick={() => setActiveTab('standings')}>
+        <div 
+          className="tab-standings" 
+          onClick={() => setActiveTab('standings')}
+          style={
+            {
+              color: activeTab === 'standings' ? 'var(--secondary-color)' : null,
+              backgroundColor: activeTab === 'standings' ? 'var(--primary-color)' : null,
+              border: activeTab === 'standings' ? '2px solid var(--secondary-color' : null
+            }}>
           <h2>Standings</h2>
         </div>
       </div>
