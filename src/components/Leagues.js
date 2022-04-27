4e6750e48fe913837d02583f95606e06bdbb4a43
TommyN987 <tomas.nagy.tn@gmail.com>
2022-04-27 17:31:45 +0200

    fetch league data from api

--- src/components/Leagues.js
+++ src/components/Leagues.js
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 const Leagues = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    
+    const fetchData = async (url) => {
+      const res = await fetch(url);
+      const apiData = await res.json();
+      return apiData.data;
+    };
+
+    fetchData('https://api-football-standings.azharimm.site/leagues')
+      .then((result) => {
+        setData(result)
+        console.log(data)
+      })
+  }, [])
+
+  console.log(data)
+
   return (
     <div className="leagues-container">
-      Leagues
+      {data.map((league) => {
+        return (
+          <div key={league.id} className='league-div'>
+            <img src={league.logos.light} alt="" />
+          </div>
+        )
+      })}
     </div>
   )
 };
